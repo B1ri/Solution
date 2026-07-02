2b6d421f23906c6b59507af0a49234e969d69d8b
Matus <matus14232@gmail.com>
2026-07-02 16:01:11 +0200

    feat: add first part of third task

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# This script prepares the environment and dependencies for the installation and configuration of Orthanc DICOM server using Ansible playbook.
+set -eu
+
+echo "Checking environment."
+
+if ! command -v apt-get >/dev/null 2>&1; then
+  echo "Sorry, this script requires Debian/Ubuntu-based system with apt."
+  exit 1
+fi
+DIR="$(cd "$(dirname "${BASH_SOURCE}")" && pwd)"
+
+
+echo "Preparing dependencies."
+
+if command -v ansible >/dev/null 2>&1; then
+  echo "Ansible is already installed."
+else
+  sudo apt update
+  echo "Installing Ansible."
+  sudo apt install -y ansible
+fi
+
+echo "Calling playbook script."
+
+
+if [ -f "$DIR/dicom_playbook.yml" ]; then
+  ansible-playbook -i "localhost," -c local "$DIR/dicom_playbook.yml"
+  else
+  echo "You need to have dicom_playbook.yml placed in same directory as setup bash file"
+  exit 1
+fi
